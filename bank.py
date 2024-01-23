1eff266c08ca75fabcfc711ab397e844ff66e07e
eedriscoll <157449079+eedriscoll@users.noreply.github.com>
2024-01-23 18:35:17 -0500

    final test

--- bank.py
+++ bank.py
@@ -1,21 +1,16 @@
 class BankAccount:
     def __init__(self, account_number):
+        self.account_number = account_number
         self.balance = 0.0
-    def deposit(self):
-        amount = float(input("Deposit an amount to the balance: "))
+    def deposit(self, amount):
         self.balance += amount
         print("Amount Deposited:", amount)
-    def withdraw(self):
-        amount = float(input("Withdraw an amount from the balance: "))
+    def withdraw(self, amount):
         if self.balance >= amount:
             self.balance -= amount
             print("Amount Withdrew:", amount)
         else:
             print("Insufficient funds")
     def display_balance(self):
-       print("Available Balance: ", self.balance)
+       print("Available Balance:", self.balance)
 
-m = BankAccount(12345)
-m.deposit()
-m.withdraw()
-m.display_balance()
